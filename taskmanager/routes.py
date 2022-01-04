681e841d2fa6c5c4afc014a764883335ff298ae8
mwarddev <martinward1981@googlemail.com>
2022-01-04 18:08:35 +0000

    Build tables

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from taskmanager import app, db
-from taskmanger.models import Category, Task
+from taskmanager.models import Category, Task
+
 
 @app.route("/")
 def home():
